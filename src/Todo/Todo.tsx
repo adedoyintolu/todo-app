cc55985d2de4f148052e40755fc065ee0dc434bb
Tolu <tolulope.adedoyin@deimos.co.za>
2020-08-13 07:45:06 +0100

    commiting the changes

--- src/Todo/Todo.tsx
+++ src/Todo/Todo.tsx
@@ -29,7 +29,7 @@ export default function Todo(){
     e.target.reset()
   }
 
-  function removeTodo(id){
+  function removeTodo(id: number){
     setTodos(todos.filter((todo) => todo.id !== id))
   }
    return(
@@ -41,7 +41,7 @@ export default function Todo(){
                 {todos.map((todo)=>(
                     <li key={todo.id} className="todos">
                         {todo.text}
-                        <button onClick={() => removeTodo(todo.id)}>X</button>
+                        <a onClick={() => removeTodo(todo.id)}>X</a>
                     </li>
                 ))}
             </ul>
@@ -49,104 +49,6 @@ export default function Todo(){
     </div>
    )
 }
-
-
-
-
-
-// import React, { useState } from "react";
-// import "../App.css";
-
-// function Todo({ todo, index, completeTodo, removeTodo }) {
-//   return (
-//     <div
-//       className="todo"
-//       style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-//     >
-//       {todo.text}
-
-//       <div>
-//         <button onClick={() => completeTodo(index)}>Complete</button>
-//         <button onClick={() => removeTodo(index)}>x</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// function TodoForm({ addTodo }) {
-//   const [value, setValue] = useState("");
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     if (!value) return;
-//     addTodo(value);
-//     setValue("");
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <input
-//         type="text"
-//         className="input"
-//         value={value}
-//         onChange={e => setValue(e.target.value)}
-//       />
-//     </form>
-//   );
-// }
-
-// function App() {
-//   const [todos, setTodos] = useState([
-//     {
-//       text: "Learn about React",
-//       isCompleted: false
-//     },
-//     {
-//       text: "Meet friend for lunch",
-//       isCompleted: false
-//     },
-//     {
-//       text: "Build really cool todo app",
-//       isCompleted: false
-//     }
-//   ]);
-
-//   const addTodo = text => {
-//     const newTodos = [...todos, { text }];
-//     setTodos(newTodos);
-//   };
-
-//   const completeTodo = index => {
-//     const newTodos = [...todos];
-//     newTodos[index].isCompleted = true;
-//     setTodos(newTodos);
-//   };
-
-//   const removeTodo = index => {
-//     const newTodos = [...todos];
-//     newTodos.splice(index, 1);
-//     setTodos(newTodos);
-//   };
-
-//   return (
-//     <div className="app">
-//       <div className="todo-list">
-//         {todos.map((todo, index) => (
-//           <Todo
-//             key={index}
-//             index={index}
-//             todo={todo}
-//             completeTodo={completeTodo}
-//             removeTodo={removeTodo}
-//           />
-//         ))}
-//         <TodoForm addTodo={addTodo} />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 
