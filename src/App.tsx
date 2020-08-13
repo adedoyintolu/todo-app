c4d00031460339c07ced1eb432568ec10d753c34
Timothy Osborne <tim@deimos.co.za>
2020-08-13 09:37:35 +0200

    Linting done

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import LoginPage from './Auth/LoginPage';
-import TodoPage from './Todo/Todo'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import TodoPage from './Todo/Todo';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import firebase from './config/firebase';
 
-const App = () =>{
+const App = () => {
   const [firebaseInitialized, setFirebaseInitialized] = useState(false);
 
   useEffect(() => {
-    firebase.isInitialized().then((val:any) => {
+    firebase.isInitialized().then((val: any) => {
       return setFirebaseInitialized(val);
     });
   });
 
-  return firebaseInitialized !==false ? (
+  return firebaseInitialized !== false ? (
     <Router>
       <Switch>
-        <Route 
-          component={LoginPage}
-          exact 
-          path="/" 
-        />
-        <Route 
-          component={TodoPage} 
-          path="/todo" 
-        />
+        <Route component={LoginPage} exact={true} path="/" />
+        <Route component={TodoPage} path="/todo" />
       </Switch>
     </Router>
-  ) : <div>You Need to Log In</div>
-} 
+  ) : (
+    <div>You Need to Log In</div>
+  );
+};
 
 export default App;
--- src/Todo/Todo.tsx
+++ src/Todo/Todo.tsx
@@ -1,66 +1,56 @@
-import React, {useState} from 'react';
-import "../App.css";
+import React, { useState } from 'react';
+import '../App.css';
 
-export default function Todo(){
-  const[newTodo, setNewTodo] = useState('')
-  const[todos, setTodos] = useState([
-      {
-          id:1,
-          text:"Buy Milk",
-          isCompleted: true
-      },
-      {
-        id:2,
-        text:"Go to the Salon",
-        isCompleted: true
+export default function Todo() {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Buy Milk',
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      text: 'Go to the Salon',
+      isCompleted: true,
+    },
+  ]);
+
+  function handleNewTodoChange(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setNewTodo(e.target.value);
+  }
+
+  function handleNewTodo(e: any) {
+    e.preventDefault();
+    if (newTodo === '') {
+      return;
     }
-  ])
-
-  function handleNewTodoChange(e: React.ChangeEvent<HTMLInputElement>){
-    e.preventDefault();
-    setNewTodo(e.target.value)
+    setTodos([...todos, { id: Date.now(), text: newTodo, isCompleted: true }]);
+    e.target.reset();
   }
 
-  function handleNewTodo(e:any){
-
-    e.preventDefault();
-    if(newTodo === '') return
-    setTodos([...todos, { id: Date.now(), text: newTodo, isCompleted:true}])
-    e.target.reset()
+  function removeTodo(id: number) {
+    setTodos(todos.filter((todo) => todo.id !== id));
   }
-
-  function removeTodo(id: number){
-    setTodos(todos.filter((todo) => todo.id !== id))
-  }
-   return(
-     <div className="app">
-         <form onSubmit={handleNewTodo} className="form">
-            <h1>Todo App</h1>
-            <input type="text" placeholder="Your todo..." onChange={handleNewTodoChange}/>
-            <ul  className="todo-list">
-                {todos.map((todo)=>(
-                    <li key={todo.id} className="todos">
-                        {todo.text}
-                        <a onClick={() => removeTodo(todo.id)}>X</a>
-                    </li>
-                ))}
-            </ul>
-         </form>
+  return (
+    <div className="app">
+      <form onSubmit={handleNewTodo} className="form">
+        <h1>Todo App</h1>
+        <input
+          type="text"
+          placeholder="Your todo..."
+          onChange={handleNewTodoChange}
+        />
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li key={todo.id} className="todos">
+              {todo.text}
+              <a onClick={() => removeTodo(todo.id)}>X</a>
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
-   )
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
